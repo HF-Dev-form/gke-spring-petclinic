dc883716dc1e557c260dee9a6412745274d33d00
FH-dev <bobykarr@gmail.com>
2023-07-11 20:57:34 +0200

    add infra workflow fix5

--- kubernetes_cluster/main.tf
+++ kubernetes_cluster/main.tf
@@ -157,7 +157,7 @@ variable "dns_name" {
 variable "preprod_environment_url" {
   description = "URL vers lb de l'api-gateway de l'environnement de pre-prod"
   type        = string
-  default     = "35.205.222.232:80"
+  default     = "35.205.222.232"
 }
 
 variable "monitoring_url" {
@@ -173,22 +173,22 @@ variable "enable_dns" {
 }
 
 
-# resource "google_dns_record_set" "preprod_record" {
-#   count        = var.enable_dns ? 1 : 0
-#   name         = "petclinic.preprod.${var.dns_name}"
-#   type         = "A"
-#   ttl          = 300
-#   rrdatas      = [var.preprod_environment_url]
-#   managed_zone = "zone-a"
-# }
+resource "google_dns_record_set" "preprod_record" {
+  count        = var.enable_dns ? 1 : 0
+  name         = "petclinic.preprod.${var.dns_name}"
+  type         = "A"
+  ttl          = 300
+  rrdatas      = [var.preprod_environment_url]
+  managed_zone = "zone-a"
+}
 
-# resource "google_dns_record_set" "monitoring_record" {
-#   count        = var.enable_dns ? 1 : 0
-#   name         = "petclinic.preprod.monitoring.${var.dns_name}"
-#   type         = "A"
-#   ttl          = 300
-#   rrdatas      = [var.monitoring_url]
-#   managed_zone = "zone-a"
-# }
+resource "google_dns_record_set" "monitoring_record" {
+  count        = var.enable_dns ? 1 : 0
+  name         = "petclinic.preprod.monitoring.${var.dns_name}"
+  type         = "A"
+  ttl          = 300
+  rrdatas      = [var.monitoring_url]
+  managed_zone = "zone-a"
+}
 
 
